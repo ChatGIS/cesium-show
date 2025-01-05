76f8423b373d0e7a3f0ee402349cb24dd93064e9
ChatGIS <ChatGIS@outlook.com>
2025-01-05 22:18:42 +0800

    v1.1.9 fix test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 
 const routes = [
+  { name: 'Test', path: '/', component: () => import('@/views/test.vue') },
   { name: 'TerrainShow', path: '/terrain-show', component: () => import('@/views/TerrainShow.vue') },
 ]
 
