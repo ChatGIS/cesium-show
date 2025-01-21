933daa746561a67f3cde17a948f2971225a9ad3a
ChatGIS <ChatGIS@outlook.com>
2025-01-21 23:00:41 +0800

    v1.6.0 add module

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ const routes = [
   { name: 'CameraParam', path: '/camera-param', component: () => import('@/views/CameraParam.vue') },
   { name: 'CustomMaterial', path: '/custom-material', component: () => import('@/views/CustomMaterial.vue') },
   { name: 'CircleWave', path: '/circle-wave', component: () => import('@/views/CircleWaveImage.vue') },
+  { name: 'HexagonSpread', path: '/hexagon-spread', component: () => import('@/views/HexagonSpread.vue') },
+  { name: 'HexagonSpreadDemo', path: '/hexagon-spread-demo', component: () => import('@/views/HexagonSpreadDemo.vue') },
 ]
 
 const router = createRouter({
