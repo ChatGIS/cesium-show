a0bb8aa0452f2773ce87d1f618c5debde646125c
ChatGIS <ChatGIS@outlook.com>
2025-01-23 23:38:50 +0800

    v1.6.3 fix router

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,9 @@ const routes = [
   { name: 'TerrainShow', path: '/terrain-show', component: () => import('@/views/TerrainShow.vue') },
   { name: 'CameraParam', path: '/camera-param', component: () => import('@/views/CameraParam.vue') },
   { name: 'CustomMaterial', path: '/custom-material', component: () => import('@/views/CustomMaterial.vue') },
-  { name: 'CircleWave', path: '/circle-wave', component: () => import('@/views/CircleWave.vue') },
+  // { name: 'CircleWave', path: '/circle-wave', component: () => import('@/views/CircleWave.vue') },
   { name: 'CircleWaveDemo', path: '/circle-wave-demo', component: () => import('@/views/CircleWaveDemo.vue') },
-  { name: 'HexagonSpread', path: '/hexagon-spread', component: () => import('@/views/HexagonSpread.vue') },
+  // { name: 'HexagonSpread', path: '/hexagon-spread', component: () => import('@/views/HexagonSpread.vue') },
   { name: 'HexagonSpreadDemo', path: '/hexagon-spread-demo', component: () => import('@/views/HexagonSpreadDemo.vue') },
 ]
 
